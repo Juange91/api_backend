ddff8a0323191e2455475769509113ddff27dfc2
Juange91 <jualgeba.91@gmail.com>
2023-05-09 20:40:18 -0500

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT,(err) => {
+if (err) {
+    console.log('Error en el servidor');
+}else{
+    console.log(`Servidor conectado en el puerto ${PORT}`);
+}
+})
+
